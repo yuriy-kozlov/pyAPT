4b27f8ae3d8a861e2e01afa6bc4daaf5e44b0257
Yuriy Kozlov <yuriy@bostonmicromachines.com>
2018-08-13 11:18:30 -0400

    pyAPT: Fixes for Python 3: message struct packs to bytes, not str

--- pyAPT/message.py
+++ pyAPT/message.py
@@ -60,7 +60,7 @@ class Message(_Message):
     assert(type(messageID) == int)
     if data:
       assert(param1 == 0 and param2 == 0)
-      assert(type(data) in [list, tuple, str])
+      assert(type(data) in [list, tuple, str, bytes])
 
       if type(data) == str:
         data = [ord(c) for c in data]
